e983e61b316ad1c1bee81bcfd1c0e5eee888e46f
Zahar <60888814+ZaharAmbrozyak@users.noreply.github.com>
2026-03-24 18:59:53 +0200

    Added usage cases in Program.cs

--- Week8-Assignment/Program.cs
+++ Week8-Assignment/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var letter1 = new Letter("1", 0.5);
+        var letter2 = new Letter("2", 0.25);
+
+        var parcel1 = new Parcel("3", 5.0, "20x10x20");
+        var parcel2 = new Parcel("4", 6.0, "30x5x40");
+        
+        letter1.PrintInfo();
+        parcel1.PrintInfo();
+
+
+        var myCargo = new CargoContainer<DeliveryItem>();
+        
+        myCargo.Add(letter1);
+        myCargo.Add(letter2);
+        myCargo.Add(parcel1);
+        myCargo.Add(parcel2);
+        
+        Console.WriteLine("Total cost = " + myCargo.GetTotalCost() + "$");
     }
 }
